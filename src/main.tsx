886922c9bfb0ae233a0ae0bd0e640676e5a9c22f
saidurr1220 <saidurr1256@gmail.com>
2025-10-13 22:56:04 +0600

    feat: Speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,19 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
